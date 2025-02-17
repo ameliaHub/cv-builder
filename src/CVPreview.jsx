63bc6d5f6196340dd0b352322f855edeb864fc86
pilarvermu <pilaribermu@gmail.com>
2025-02-17 10:43:09 +0100

    Añadido contenedor cv-preview-inner

--- src/CVPreview.jsx
+++ src/CVPreview.jsx
@@ -10,52 +10,54 @@ function CvSection({ title, children, className }) {
 function CvPreview({ generalInfo, education, experience }) {
   return (
     <div className="cv-preview">
-      <CvSection title={generalInfo.userName} className="general-info">
-        <p>
-          <strong>{generalInfo.email}</strong>
-        </p>
-        <p>
-          <strong>{generalInfo.phone}</strong>
-        </p>
-      </CvSection>
+      <div className="cv-preview-inner">
+        <CvSection title={generalInfo.userName} className="general-info">
+          <p>
+            <strong>{generalInfo.email}</strong>
+          </p>
+          <p>
+            <strong>{generalInfo.phone}</strong>
+          </p>
+        </CvSection>
 
-      <CvSection title="Educación" className="education">
-        <div className="education-container">
-          {education.map((educ, index) => (
-            <div key={index} className="education-item">
-              <div className="date">
-                <p className="startDate">{educ.startDate}</p>
-                <p className="endDate">{educ.endDate}</p>
+        <CvSection title="Educación" className="education">
+          <div className="education-container">
+            {education.map((educ, index) => (
+              <div key={index} className="education-item">
+                <div className="date">
+                  <p className="startDate">{educ.startDate}</p>
+                  <p className="endDate">{educ.endDate}</p>
+                </div>
+
+                <p>
+                  <strong>{educ.school}</strong>
+                </p>
+                <p>{educ.location}</p>
+                <p>{educ.degree}</p>
               </div>
+            ))}
+          </div>
+        </CvSection>
 
-              <p>
-                <strong>{educ.school}</strong>
-              </p>
-              <p>{educ.location}</p>
-              <p>{educ.degree}</p>
-            </div>
-          ))}
-        </div>
-      </CvSection>
-
-      <CvSection title="Experiencia" className="experience">
-        <div className="experience-container">
-          {experience.map((exp, index) => (
-            <div key={index} className="experience-item">
-              <div className="date">
-                <p className="startDate">{exp.startDate}</p>
-                <p className="endDate">{exp.endDate}</p>
+        <CvSection title="Experiencia" className="experience">
+          <div className="experience-container">
+            {experience.map((exp, index) => (
+              <div key={index} className="experience-item">
+                <div className="date">
+                  <p className="startDate">{exp.startDate}</p>
+                  <p className="endDate">{exp.endDate}</p>
+                </div>
+                <p>
+                  <strong>{exp.company}</strong>
+                </p>
+                <p>{exp.location}</p>
+                <p>{exp.position}</p>
+                <p className="experience-desc">{exp.description}</p>
               </div>
-              <p>
-                <strong>{exp.company}</strong>
-              </p>
-              <p>{exp.location}</p>
-              <p>{exp.position}</p>
-              <p className="experience-desc">{exp.description}</p>
-            </div>
-          ))}
-        </div>
-      </CvSection>
+            ))}
+          </div>
+        </CvSection>
+      </div>
     </div>
   );
 }
